980e6f0f76fd8f8653186dc5a3743e9d021b7eee
Brian McDonald <bmcdonald05@gmail.com>
2016-04-27 14:23:41 -0400

    hangman mostly done

--- hangman.rb
+++ hangman.rb
@@ -11,23 +11,26 @@ def get_word
 	@word = dictionary.sample
 	p @word
 	@word_arr = @word.scan(/./)
-	#p @word_arr
+	@word_arr2 = @word.scan(/./)
 	puts "The word has #{@word.length} letters in it."
 end
 
 def guess_game
-	puts "Guess a letter"
-	guess = gets.chomp.downcase
-	puts "You guessed #{guess}"
-	if @word_arr.include?(guess)
-		puts "#{guess} is indeed one of the letters!"
-		@correct_ans +=1
-	else
-		puts "#{guess} is not one of the letters."
-		@tries -=1
+	until @tries == 0 || @word_arr2.length == 0
+		puts "Guess a letter"
+		guess = gets.chomp.downcase
+		puts "You guessed #{guess}"
+		if @word_arr.include?(guess)
+			puts "#{guess} is indeed one of the letters!"
+			@correct_ans +=1
+			@word_arr2.delete_if {|x| x == guess}
+		else
+			puts "#{guess} is not one of the letters."
+			@tries -=1
+		end
+		puts "You have guessed #{@correct_ans} of the #{@word.length} letters, and have #{@tries} attempts left"
+		guess_game
 	end
-	puts "You have guessed #{@correct_ans} of the #{@word.length} letters, and have #{@tries} attempts left"
-	guess_game
 end
 
 @correct_ans = 0
@@ -35,18 +38,22 @@ end
 intro
 get_word
 guess_game
+if @word_arr2.length == 0
+	puts "You guessed the word! it was '#{@word}'"
+else
+	puts "You didn't guess the word, it was #{@word}. Look what you did!!!"
+	puts  '
+________
+|      |
+|      O
+|     \|/ 
+|      |
+|     / \
+|
+'
+end
 
 
 
 
 
-
-#  "
-# ________
-# |      |
-# |      O
-# |     \|/ 
-# |      |
-# |     / \
-# |
-# "
