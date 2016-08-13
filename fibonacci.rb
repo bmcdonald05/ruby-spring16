cddf92ea223d4a243cd7536d9862d377c91b92a1
Brian McDonald <bmcdonald05@gmail.com>
2016-08-13 16:37:04 -0400

    fibb sequence finished

--- fibonacci.rb
+++ fibonacci.rb
@@ -1,15 +1,27 @@
-# def fib(n)
-#   n.times.each_with_object([0,1]) { |num, obj| obj << obj[-2] + obj[-1] }
-# end
-# puts fib(10)
-
-
+n = 9
 def fib(n)
   if n == 0 || n == 1
-    puts n
+    return n
   else
-    puts (fib(n-1)) += (fib(n-2))
+    return fib(n-1) + (fib(n-2))
   end
 end
 
-puts fib(8)
+
+puts "#{fib(n)}"
+
+
+# def fibonacci(n)
+#   if n == 1
+#     1
+#   elsif n == 2
+#     1
+#   else
+#     fibonacci(n-1) + fibonacci(n-2)
+#   end
+# end
+
+
+
+
+# 0, 1, 1, 2, 3, 5, 8, 13, 21
